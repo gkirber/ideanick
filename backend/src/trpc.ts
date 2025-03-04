b2c3e291e84c0827c0ca550dcad575147c707791
gkirber <george.kirber@gmail.com>
2025-03-04 23:29:42 +0100

    Fix Husky issue

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,21 +1,30 @@
 import { initTRPC } from '@trpc/server'
-const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Idea 1 description...' },
-  { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Idea 2 description...' },
-  { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Idea 3 description...' },
-  { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Idea 4 description...' },
-  { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Idea 5 description...' },
-]
+import _ from 'lodash'
+import { z } from 'zod'
 
-const x: string = 'hello123'
-if (Math.random() + 1) console.info(x)
+const ideas = _.times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}`,
+  text: _.times(100, (j) => `<p>Text paragraph ${j} of idea ${i}...</p>`).join(''),
+}))
+
+console.log(ideas)
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
-    return { ideas }
+    return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) }
   }),
+  getIdea: trpc.procedure.input(
+    z.object({
+      ideaNick: z.string(),
+    }),
+  ).query(({input}) => {
+    const idea = ideas.find((idea) => idea.nick === input.ideaNick)
+    return {idea: idea || null}
+  })
 })
 
 export type TrpcRouter = typeof trpcRouter
