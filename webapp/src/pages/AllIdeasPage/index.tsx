682c18712d682130a24a50f03971f53c19c70eb6
gkirber <george.kirber@gmail.com>
2025-03-17 22:50:54 +0100

    Creating reusable components

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -2,7 +2,7 @@ import { getViewIdeaRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 import { Link } from 'react-router-dom'
 import css from './index.module.scss'
-
+import { Segment } from '../../components/Segment'
 
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
@@ -20,20 +20,22 @@ export const AllIdeasPage = () => {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>All Ideas</h1>
+    <Segment title="All Ideas">
       <div className={css.ideas}>
         {data.ideas.map((idea) => (
           <div className={css.idea} key={idea.nick}>
-            <h2 className={css.ideaName}>
-              <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
-                {idea.name}
-              </Link>
-            </h2>
-            <p className={css.ideaDescription}>{idea.description}</p>
+            <Segment
+              size={2}
+              title={
+                <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                  {idea.name}
+                </Link>
+              }
+              description={idea.description}
+            />
           </div>
         ))}
       </div>
-    </div>
+    </Segment>
   )
 }
