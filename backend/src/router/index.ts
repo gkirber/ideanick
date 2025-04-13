8301366b2c5beee6cd44ba0c641628e3c5b06533
gkirber <george.kirber@gmail.com>
2025-04-13 15:58:32 +0200

    Automatic generation of index file

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,10 +1,13 @@
 import { trpc } from '../lib/trpc'
-import { getIdeaTrpcRoute } from './getIdea'
-import { getIdeasTrpcRoute } from './getIdeas'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
+
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getIdea: getIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
+  // @endindex
 })
 
 export type TrpcRouter = typeof trpcRouter
