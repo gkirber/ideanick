2d4aef8ad1e5e63d28028fe5591a7abbeeba17a0
gkirber <george.kirber@gmail.com>
2025-05-12 22:15:51 +0200

    We make a wrapper for creating routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,19 +1,24 @@
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import { Layout } from './components/Layout'
 import { AppContextProvider } from './lib/ctx'
 import * as routes from './lib/routes'
 import { TrpcProvider } from './lib/trpc'
+
 import { EditProfilePage } from './pages/auth/EditProfilePage'
 import { SignInPage } from './pages/auth/SignInPage'
+import { SignOutPage } from './pages/auth/SignOutPage'
 import { SignUpPage } from './pages/auth/SignUpPage'
+
+import { AllIdeasPage } from './pages/ideas/AllIdeasPage'
 import { EditIdeaPage } from './pages/ideas/EditIdeaPage'
 import { NewIdeaPage } from './pages/ideas/NewIdeaPage'
 import { ViewIdeaPage } from './pages/ideas/ViewIdeaPage'
+
 import { NotFoundPage } from './pages/other/NotFoundPage'
+
 import './styles/global.scss'
-import { SignOutPage } from './pages/auth/SignOutPage'
-import { AllIdeasPage } from './pages/ideas/AllIdeasPage'
 
 export const App = () => {
   return (
@@ -22,15 +27,15 @@ export const App = () => {
         <AppContextProvider>
           <BrowserRouter>
             <Routes>
-              <Route path={routes.getSignOutRoute()} element={<SignOutPage />} />
+              <Route path={routes.getSignOutRoute.definition} element={<SignOutPage />} />
               <Route element={<Layout />}>
-                <Route path={routes.getSignUpRoute()} element={<SignUpPage />} />
-                <Route path={routes.getSignInRoute()} element={<SignInPage />} />
-                <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
-                <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
-                <Route path={routes.getEditProfileRoute()} element={<EditProfilePage />} />
-                <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
-                <Route path={routes.getEditIdeaRoute(routes.editIdeaRouteParams)} element={<EditIdeaPage />} />
+                <Route path={routes.getSignUpRoute.definition} element={<SignUpPage />} />
+                <Route path={routes.getSignInRoute.definition} element={<SignInPage />} />
+                <Route path={routes.getEditProfileRoute.definition} element={<EditProfilePage />} />
+                <Route path={routes.getAllIdeasRoute.definition} element={<AllIdeasPage />} />
+                <Route path={routes.getViewIdeaRoute.definition} element={<ViewIdeaPage />} />
+                <Route path={routes.getEditIdeaRoute.definition} element={<EditIdeaPage />} />
+                <Route path={routes.getNewIdeaRoute.definition} element={<NewIdeaPage />} />
                 <Route path="*" element={<NotFoundPage />} />
               </Route>
             </Routes>
