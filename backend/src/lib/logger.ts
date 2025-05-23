70da1af17bbc0276b54a8806ae02adfbc9257eb0
gkirber <george.kirber@gmail.com>
2025-05-23 20:52:58 +0200

    Logging: Beautiful Log Output During Development

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,20 +1,67 @@
+import { EOL } from 'os'
+import _ from 'lodash'
+import pc from 'picocolors'
 import { serializeError } from 'serialize-error'
-import winston from 'winston'
+import { MESSAGE } from 'triple-beam'
+import winston, { format } from 'winston'
+import * as yaml from 'yaml'
 import { env } from './env'
+
+
+type TransformableInfo = {
+  level: string
+  message: string
+  timestamp?: string
+  logType?: string
+  [key: string]: any
+}
 
 export const winstonLogger = winston.createLogger({
   level: 'debug',
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    winston.format.errors({ stack: true }),
-    winston.format.json()
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.errors({ stack: true }),
+    format.json()
   ),
   defaultMeta: { service: 'backend', hostEnv: env.HOST_ENV },
   transports: [
     new winston.transports.Console({
-      format: winston.format.json(),
+      format:
+        env.HOST_ENV !== 'local'
+          ? format.json()
+          : format((infoRaw) => {
+              const info = infoRaw as TransformableInfo
+
+              const setColor = {
+                info: (str: string) => pc.blue(str),
+                error: (str: string) => pc.red(str),
+                debug: (str: string) => pc.cyan(str),
+              }[info.level as 'info' | 'error' | 'debug'] || ((str: string) => str)
+
+              const levelAndType = `${info.level} ${info.logType ?? ''}`.trim()
+              const topMessage = `${setColor(levelAndType)} ${pc.green(info.timestamp ?? '')}${EOL}${info.message ?? ''}`
+
+              const visibleMessageTags = _.omit(info, [
+                'level',
+                'logType',
+                'timestamp',
+                'message',
+                'service',
+                'hostEnv',
+              ])
+
+              const stringifiedMeta = _.trim(
+                yaml.stringify(visibleMessageTags, (_k, v) => (_.isFunction(v) ? 'Function' : v))
+              )
+
+              return {
+                ...info,
+                message: info.message ?? '', // важливо для сумісності з Winston
+                [MESSAGE]: [topMessage, Object.keys(visibleMessageTags).length > 0 ? `${EOL}${stringifiedMeta}` : '']
+                  .filter(Boolean)
+                  .join('') + EOL,
+              }
+            })(),
     }),
   ],
 })
@@ -23,7 +70,7 @@ export const logger = {
   info: (logType: string, message: string, meta?: Record<string, any>) => {
     winstonLogger.info(message, { logType, ...meta })
   },
-  error: (logType: string, error: any, meta?: Record<string, any>) => {
+  error: (logType: string, error: unknown, meta?: Record<string, any>) => {
     const serializedError = serializeError(error)
     winstonLogger.error(serializedError.message || 'Unknown error', {
       logType,
