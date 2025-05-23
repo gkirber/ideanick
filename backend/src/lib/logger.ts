ee6c1f2c0da5fdd170634e8912ac1a5bc836a2f6
gkirber <george.kirber@gmail.com>
2025-05-23 22:08:55 +0200

    Logging: Secret Data

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -6,8 +6,8 @@ import { serializeError } from 'serialize-error'
 import { MESSAGE } from 'triple-beam'
 import winston, { format } from 'winston'
 import * as yaml from 'yaml'
+import { deepMap } from '../utils/deepMap'
 import { env } from './env'
-
 
 type TransformableInfo = {
   level: string
@@ -33,11 +33,12 @@ export const winstonLogger = winston.createLogger({
           : format((infoRaw) => {
               const info = infoRaw as TransformableInfo
 
-              const setColor = {
-                info: (str: string) => pc.blue(str),
-                error: (str: string) => pc.red(str),
-                debug: (str: string) => pc.cyan(str),
-              }[info.level as 'info' | 'error' | 'debug'] || ((str: string) => str)
+              const setColor =
+                {
+                  info: (str: string) => pc.blue(str),
+                  error: (str: string) => pc.red(str),
+                  debug: (str: string) => pc.cyan(str),
+                }[info.level as 'info' | 'error' | 'debug'] || ((str: string) => str)
 
               const levelAndType = `${info.level} ${info.logType ?? ''}`.trim()
               const topMessage = `${setColor(levelAndType)} ${pc.green(info.timestamp ?? '')}${EOL}${info.message ?? ''}`
@@ -58,23 +59,34 @@ export const winstonLogger = winston.createLogger({
               return {
                 ...info,
                 message: info.message ?? '', // важливо для сумісності з Winston
-                [MESSAGE]: [topMessage, Object.keys(visibleMessageTags).length > 0 ? `${EOL}${stringifiedMeta}` : '']
-                  .filter(Boolean)
-                  .join('') + EOL,
+                [MESSAGE]:
+                  [topMessage, Object.keys(visibleMessageTags).length > 0 ? `${EOL}${stringifiedMeta}` : '']
+                    .filter(Boolean)
+                    .join('') + EOL,
               }
             })(),
     }),
   ],
 })
 
+type Meta = Record<string, any> | undefined
+const prettifyMeta = (meta: Meta): Meta => {
+  return deepMap(meta, ({ key, value }) => {
+    if (['email', 'password', 'newPassword', 'oldPassword', 'token', 'text', 'description'].includes(key)) {
+      return '🙈'
+    }
+    return value
+  })
+}
+
 export const logger = {
-  info: (logType: string, message: string, meta?: Record<string, any>) => {
+  info: (logType: string, message: string, meta?: Meta) => {
     if (!debug.enabled(`ideanick:${logType}`)) {
       return
     }
-    winstonLogger.info(message, { logType, ...meta })
+    winstonLogger.info(message, { logType, ...prettifyMeta(meta) })
   },
-  error: (logType: string, error: unknown, meta?: Record<string, any>) => {
+  error: (logType: string, error: any, meta?: Meta) => {
     if (!debug.enabled(`ideanick:${logType}`)) {
       return
     }
@@ -83,7 +95,7 @@ export const logger = {
       logType,
       error,
       errorStack: serializedError.stack,
-      ...meta,
+      ...prettifyMeta(meta),
     })
   },
 }
