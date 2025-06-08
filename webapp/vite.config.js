2bffb834dae676c2f15c822865e8418bcdceaf5c
gkirber <george.kirber@gmail.com>
2025-06-08 12:46:30 +0200

    Frontend Bundle Analysis

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -3,19 +3,16 @@ import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
-
 import { parsePublicEnv } from './src/lib/parsePublicEnv'
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const publicEnv = parsePublicEnv(env)
-
+export default defineConfig(function (_a) {
+  var mode = _a.mode
+  var env = loadEnv(mode, process.cwd(), '')
+  var publicEnv = parsePublicEnv(env)
   if (env.HOST_ENV !== 'local' && env.SENTRY_AUTH_TOKEN) {
     if (!env.SOURCE_VERSION) {
       throw new Error('SOURCE_VERSION is not defined')
     }
   }
-
   return {
     plugins: [
       react(),
